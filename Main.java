7f34bb722113c837188228dc240771fe68db6438
6Schetko9 <105492502+6Schetko9@users.noreply.github.com>
2022-06-28 20:49:39 +0300

    №1(2806)

---
+++ Main.java
@@ -0,0 +1,70 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*//Begin1.Дана  сторона а
+        // ввод или присвоение исходных данный
+        int a = 5;
+        // вычисление результата
+        int P = 4 * a;
+        //вывод результата
+        System.out.println(P);
+    }
+}
+*/
+
+
+// begin 2.  дана а найти площадь
+/*        int a = 5;
+// вычислить S
+        int S = a * a;
+        system.out.println("У квадрата со стророной " + a + " площадь " + S);
+
+    }
+}
+
+        */
+
+// Begin4. дан диаметр d.Найти ее длинну L= pi*d/
+//пи  =3.14
+      /*  int d = 10;
+        final double pi = 3.14;
+        double L = pi * d;
+        System.out.println(" У круга с диаметром " + d + " длина окружности " + L);
+        */
+
+// Begim5. Дана длина ребра куба a. Найти объем куба V = a^3 и S=6*a^2
+/*int a = 10;
+// Вычислить V
+int V = a*a*a;
+// Вычислить S
+int S = 6*a*a;
+// вывести S
+System.out.println("У куба с ребром " +V);
+System.out.println ("Площ:"+S);
+*/
+/*int a=2;
+int b=3;
+int c= 4;
+int V= a*b*c;
+int S= 2*(a*b+b*c+c*a);
+        System.out.println("Площадь" +S);
+        System.out.println("Объем" + V);
+        */
+
+
+   //Begin8
+        /*int a=3;
+    int b=4;
+        int d=2;
+    double c = (a+b)/d;
+           System.out.println("вывод" +c);
+           */
+// Begin a
+        int a>=0;
+        int b>=0;
+        int c=a+b;
+        System.out.println("сумма"+ c);
+    }
+}
